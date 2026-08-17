fb3efe0967e106b2f2b48683f9da5cc6fa639840
Lakshya Acharya <lakshyaacharya08@gmail.com>
2026-08-17 17:57:38 +0530

    Fix custom login

--- studentmanagment/urls.py
+++ studentmanagment/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from students import views
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -51,17 +51,7 @@ urlpatterns = [
         name='student_detail'
     ),
 
-    path(
-        'login/',
-        auth_views.LoginView.as_view(
-            template_name='login.html'
-        ),
-        name='login'
-    ),
+path('login/', views.login_user, name='login'),
 
-    path(
-        'logout/',
-        auth_views.LogoutView.as_view(),
-        name='logout'
-    ),
+path('logout/', views.logout_user, name='logout'),
 ]
--- students/views.py
+++ students/views.py
@@ -3,6 +3,7 @@ from .models import Student
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 @login_required
@@ -99,3 +100,31 @@ def student_detail(request, id):
     return render(request, 'student_detail.html', {
         'student': student
     })
+
+
+def login_user(request):
+
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+        return render(request, "login.html", {
+            "error": "Invalid username or password"
+        })
+
+    return render(request, "login.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
